62b8ba9c93f570489079370d0b9f78913d7637e7
vanessaruama <vanessaruama@gmail.com>
2021-08-26 22:44:33 -0300

    Aula03 - Começo de código com JS, trabalhando com listas no html, salvando informações na localStorage

---
+++ Aula03/js/main.js
@@ -0,0 +1,46 @@
+// Olá Javascript
+
+// Vai rodar quando a janela abrir
+onload = function() {
+    const fieldlogin = document.getElementById("login")
+    const fieldpassword = document.getElementById("password")
+    const fieldsavepsw = document.getElementById("savepsw")
+    
+    if (window.localStorage.getItem("savepsw")) {
+        fieldlogin.value = window.localStorage.getItem("login")
+        fieldpassword.value = window.localStorage.getItem("password")
+        fieldsavepsw.checked = window.localStorage.getItem("savepsw")
+    }
+}    
+
+function doLogin() {   
+    // Tipo nome = valor
+    const fieldlogin = document.getElementById("login")
+    const fieldpassword = document.getElementById("password")
+    const fieldsavepsw = document.getElementById("savepsw")
+    
+    //--teste-- console.log(fieldlogin.value)
+    //--teste-- console.log(fieldpassword.value)
+    //--teste-- console.log(fieldsavepsw.checked)
+
+    const ischeckedsavepsw = fieldsavepsw.checked
+
+    if (ischeckedsavepsw) {
+        //Cria/Salva no localStorage
+        window.localStorage.setItem("login",  fieldlogin.value)
+        window.localStorage.setItem("password",  fieldpassword.value)
+        window.localStorage.setItem("savepsw",  fieldpassword.checked)
+    } else {
+        //Remove do localStorage
+        window.localStorage.removeItem("login")
+        window.localStorage.removeItem("password")
+        window.localStorage.removeItem("savepsw")
+    }
+}
+
+/*
+localStorage.setItem() para criar um novo par de chave: valor
+localStorage.getItem() para recuperar o valor do par chave: valor
+localStorage.romoveItem() para remover um par específico
+localStorage.clear() apaga TODOS os pares gravados pra aquele domínio
+*/
