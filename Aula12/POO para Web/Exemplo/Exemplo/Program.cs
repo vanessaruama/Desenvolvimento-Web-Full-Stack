88d8f1d8a148a0ce7816539191f43fba9cf8acc2
vanessaruama <vanessaruama@gmail.com>
2021-11-11 00:17:50 -0300

    adição fontes da aula

---
+++ Aula12/POO para Web/Exemplo/Exemplo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bolinha bola = new Bolinha();
+            bola.VirouEmoji("sorriso");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+
+    class Cor
+    {
+        string nome;
+    }
+
+    class Forma
+    {
+        Cor cor;
+    }
+
+    class Coracao : Forma
+    {
+        public void Bater()
+        {
+            Console.WriteLine("Coracao bateu");
+        }
+    }
+
+    class Triangulo : Forma
+    {
+        public void AchouAMumia()
+        {
+            Console.WriteLine("Achou a Mumia");
+        }
+    }
+
+    class Bolinha : Forma 
+    {
+        public void VirouEmoji(string nome)
+        {
+            Console.WriteLine("Bolinha virou emoji" + nome);
+        }
+    }
+
+    class Quadrado: Forma
+    {
+        public void VirouPiscina()
+        {
+            Console.WriteLine("Virou Piscina");
+        }
+    }
+}
